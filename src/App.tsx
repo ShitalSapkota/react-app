49b0d52e6d021f6fe2bbf2c9e1d95c5544dc8827
Shital <shital.sapkota1@gmail.com>
2023-10-02 22:07:05 +0300

    List Groups

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Message from "./Messages";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /> </div>
+  return <div><ListGroup /> </div>
 }
 
 export default App;
