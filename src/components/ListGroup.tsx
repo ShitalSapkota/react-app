b94759afd42652ded88cbdf38909ee26752feafb
Shital <shital.sapkota1@gmail.com>
2023-10-04 21:11:35 +0300

    new line created

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,43 @@
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  // State Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // example: arr[0]; //variable (selectedIndex)
+  // arr[1]; // updater function
+
+  // Event Handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
-      <h1>List </h1>
+      <h1>{heading}</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            // onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
